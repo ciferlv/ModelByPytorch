f4e9f66860beb7f5a1becccb2111c5560d054489
ciferlv <xzlv.nju@gmail.com>
2018-12-30 09:54:12 +0800

    update

--- LDA/LDA_scratch.py
+++ LDA/LDA_scratch.py
@@ -1,16 +1,16 @@
+import nltk
 import nltk.stem
+import numpy as np
 from gensim.corpora import Dictionary
 from gensim.models import LdaModel
 from nltk.corpus import stopwords
 
-import numpy as np
-
 my_stem = nltk.stem.SnowballStemmer('english')
 
-raw_data_file = "./data/news.txt"
-filtered_data_file = './data/news_filtered.txt'
+raw_data_file = "LDA/data/news.txt"
+# filtered_data_file = './data/news_filtered.txt'
+filtered_data_file = 'LDA/data/news_filtered.txt'
 
-import nltk
 nltk.download('stopwords')
 stopWords = set(stopwords.words('english'))
 
@@ -41,7 +41,7 @@ def reform_data():
             f.write("{}\n".format(line))
 
 
-def lda_sklearn():
+def my_lda_learn(topic):
     text = []
     with open(filtered_data_file, 'r', encoding="UTF-8") as f:
         for line in f.readlines():
@@ -50,9 +50,10 @@ def lda_sklearn():
 
     text2bow = [dictionary.doc2bow(one_text) for one_text in text]
 
-    my_lda = LdaModel(text2bow, id2word=dictionary, num_topics=20, passes=20)
+    my_lda = LdaModel(
+        text2bow, id2word=dictionary, num_topics=topic, passes=20)
 
-    print(my_lda.print_topics(num_topics=5, num_words=10))
+    print(my_lda.print_topics(num_topics=topic, num_words=10))
 
 
 def lda_scratch(topic_num, alpha, beta, passes):
@@ -75,7 +76,7 @@ def lda_scratch(topic_num, alpha, beta, passes):
 
     n_d_k = np.zeros((doc_num, topic_num))
     n_k_w = np.zeros((topic_num, word_num))
-    n_k = np.zeros((topic_num,))
+    n_k = np.zeros((topic_num, ))
 
     z = {}
 
@@ -87,15 +88,16 @@ def lda_scratch(topic_num, alpha, beta, passes):
             n_k[k] += 1
             z[(d, w_index)] = k
 
-    n_d_k = n_d_k + np.ones((topic_num,)) * alpha
-    n_k_w = n_k_w + np.ones((word_num,)) * beta
+    n_d_k = n_d_k + np.ones((topic_num, )) * alpha
+    n_k_w = n_k_w + np.ones((word_num, )) * beta
 
     theta = np.zeros((doc_num, topic_num))
 
     for i_pass in range(passes):
         print("I_Pass: {}".format(i_pass))
         for d, doc in enumerate(docs_idx_list):
-            theta[d] = np.random.dirichlet(n_d_k[d] + np.ones((topic_num,)) * alpha, 1)
+            theta[d] = np.random.dirichlet(
+                n_d_k[d] + np.ones((topic_num, )) * alpha, 1)
             for w_index, w in enumerate(doc):
                 word = w
                 topic = z[(d, w_index)]
@@ -107,7 +109,8 @@ def lda_scratch(topic_num, alpha, beta, passes):
                 p_z_k = n_d_k[d] * temp_phi
                 # p_z_k = theta[d] * temp_phi
 
-                new_topic = np.random.multinomial(1, p_z_k / np.sum(p_z_k)).argmax()
+                new_topic = np.random.multinomial(
+                    1, p_z_k / np.sum(p_z_k)).argmax()
 
                 z[(d, w_index)] = new_topic
                 n_d_k[d, new_topic] += 1
@@ -118,13 +121,18 @@ def lda_scratch(topic_num, alpha, beta, passes):
         print("K: {}".format(k_i))
         arg_list = (n_k_w[k_i]).argsort()[-10:]
         for idx, arg_index in enumerate(list(reversed(arg_list))):
-            print("{}: {}".format(dictionary[arg_index], n_k_w[k_i, arg_index] / np.sum(n_k_w[k_i])), end="\t")
+            print(
+                "{}: {}".format(dictionary[arg_index],
+                                n_k_w[k_i, arg_index] / np.sum(n_k_w[k_i])),
+                end="\t")
         print("\n")
 
 
 if __name__ == "__main__":
     # reform_data()
-    # lda_sklearn()
+    my_lda_learn(5)
+    # lda_sklearn(10)
+    # lda_sklearn(20)
     # lda_scratch(5, 0.5, 0.5, 20)
     # lda_scratch(10, 0.5, 0.5,20)
-    lda_scratch(20, 0.5, 0.5,20)
+    # lda_scratch(20, 0.5, 0.5,20)
